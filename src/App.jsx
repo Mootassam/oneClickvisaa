0cbf71536efd1ead30683080b9f869d5f284c41d
Mootassam <mootassame@gmail.com>
2024-02-21 17:21:07 +0400

    add the collapse

---
+++ src/App.jsx
@@ -0,0 +1,441 @@
+import "./App.css";
+import React, { useEffect, useRef, useState } from "react";
+
+function App() {
+  const [active, setActive] = useState(false);
+ 
+
+  const toggle = () => {
+    setActive(!active);
+  };
+  useEffect(() => {
+  }, [active]);
+  return (
+    <>
+      <>
+        <header className="header">
+          <div className="navbar-area">
+            <div className="container">
+              <div className="row align-items-center">
+                <div className="col-lg-12">
+                  <nav className="navbar navbar-expand-lg">
+                    <a className="navbar-brand" href="#home">
+                      <img src="assets/images/logo/logo.svg" alt="Logo" />
+                    </a>
+                    <button
+                      className={`navbar-toggler ${active ? "active" : ""}`}
+                      onClick={() => {
+                        toggle();
+                      }}
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#navbarSupportedContent"
+                      aria-controls="navbarSupportedContent"
+                      aria-expanded="false"
+                      aria-label="Toggle navigation"
+                    >
+                      <span className="toggler-icon" />
+                      <span className="toggler-icon" />
+                      <span className="toggler-icon" />
+                    </button>
+                    <div
+                      className={`collapse navbar-collapse sub-menu-bar ${
+                        active ? "show" : ""
+                      }`}
+                      id="navbarSupportedContent"
+                    >
+                      <div className="ms-auto">
+                        <ul id="nav" className="navbar-nav ms-auto">
+                          <li className="nav-item" onClick={()=>setActive(false)}>
+                            <a className="page-scroll active" href="#home">
+                              Home
+                            </a>
+                          </li>
+                          <li className="nav-item" onClick={()=>setActive(false)}>
+                            <a className="page-scroll" href="#features">
+                              Steps
+                            </a>
+                          </li>
+                          <li className="nav-item" onClick={()=>setActive(false)}>
+                            <a className="page-scroll" href="#about">
+                              Process
+                            </a>
+                          </li>
+                          <li className="nav-item" onClick={()=>setActive(false)}>
+                            <a className="page-scroll" href="#pricing">
+                              Pricing
+                            </a>
+                          </li>
+                          {/* <li class="nav-item">
+                  <a class="page-scroll" href="#team">Team</a>
+                </li> */}
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="header-btn">
+                      <a href="#0" className="main-btn btn-hover">
+                        Apply Now !!
+                      </a>
+                    </div>
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+        <section id="home" className="hero-section">
+          <img
+            src="/assets/images/background.png"
+            alt="FlagUAE"
+            className="flag"
+          />
+          <img
+            src="/assets/images/border.png"
+            alt="borderTop"
+            className="image__border"
+          />
+          <img
+            src="/assets/images/backgroundmobile.png"
+            alt="homeagebackground"
+            className="mobilebackground"
+          />
+          <div className="hero-wrapper">
+            <div className="container">
+              <div className="row align-items-center">
+                <div className="col-xxl-5 col-xl-6 col-lg-6 col-md-10">
+                  <div className="hero-content">
+                    <h1>Apply UAE Visa Online</h1>
+                    <p>
+                      Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                      sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                      magna aliquyam erat, sed diam voluptua.
+                    </p>
+                    <a href="#features" className="main-btn btn-hover">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+                <div className="col-xxl-6 col-xl-6 col-lg-6 offset-xxl-1">
+                  <div className="hero-image text-center text-lg-start"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="features" className="feature-section">
+          <div className="feature-wrapper">
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-xxl-6 col-xl-7 col-lg-8 col-md-11">
+                  <div className="section-title text-center mb-60">
+                    <h2>
+                      Only 3 Steps <br className="d-block" />
+                      to Apply Visa
+                    </h2>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-lg-4 col-md-6">
+                  <div className="single-feature-wrapper">
+                    <div className="single-feature">
+                      <div className="feature-icon color-1">
+                        <i className="lni lni-display size" />
+                      </div>
+                      <div className="feature-content">
+                        <h4>Apply Online</h4>
+                        <p>
+                          You can apply online anytime from anywhere in 3 simple
+                          steps.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-4 col-md-6">
+                  <div className="single-feature-wrapper">
+                    <div className="single-feature">
+                      <div className="feature-icon color-2">
+                        <i className="lni lni-credit-cards size" />
+                      </div>
+                      <div className="feature-content">
+                        <h4>Pay Online</h4>
+                        <p>
+                          You can pay online with debit or credit card sdfgsd.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-4 col-md-6">
+                  <div className="single-feature-wrapper">
+                    <div className="single-feature">
+                      <div className="feature-icon color-3">
+                        <i className="lni lni-envelope size" />
+                      </div>
+                      <div className="feature-content">
+                        <h4>Get Visa Online</h4>
+                        <p>
+                          Get e-Visa by email. You just need to print and
+                          travel.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="about" className="about-section">
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-lg-6 order-last order-lg-first">
+                <div className="about-image text-center text-lg-end">
+                  <img src="assets/images/pay.jpg" alt="pay" />
+                </div>
+              </div>
+              <div className="col-lg-6 col-md-10">
+                <div className="about-content-wrapper space">
+                  <div className="numbers">1</div>
+                  <div className="section-title">
+                    <h2 className="mb-20">
+                      Apply Online Dubai Visa Application
+                    </h2>
+                    <p className="mb-30">
+                      Simple application form: <br />
+                      1. Choose your nationality (Your country) <br />
+                      2. Choose type of Dubai visa (Single or multi entry visa)
+                      <br />
+                      3. Choose Visa option according to stay duration
+                      <br />
+                      4. Fill the contact details <br />
+                      5. Choose processing time
+                    </p>
+                    <a href="#0" className="main-btn btn-hover">
+                      <i className="lni lni-whatsapp" /> WhatsApp Now !!
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="feature-2" className="feature-section-2 payonline">
+          <div className="feature-wrapper-2">
+            <div className="container">
+              <div className="row align-items-center">
+                <div className="col-lg-6 col-md-10">
+                  <div className="feature-content-wrapper space">
+                    <div className="numbers">2</div>
+                    <div className="section-title">
+                      <h2 className="mb-30">
+                        Upload Documents <br className="d-none d-sm-block" />
+                        for Dubai Visa Application
+                      </h2>
+                      <p className="mb-30 text__align">
+                        <span>
+                          1. Upload Passport (Scanned copy or take clear picture
+                          with mobile phone).
+                        </span>{" "}
+                        &nbsp;
+                        <span />
+                        2. White background picture of applicant (Take Selfie
+                        standing in front of white wall)
+                      </p>
+                      <a href="#0" className="main-btn btn-hover">
+                        <i className="lni lni-whatsapp" /> whatsApp Now!!
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="feature-image text-lg-end">
+                    <img
+                      src="assets/images/upload.png"
+                      alt=""
+                      style={{ inlineSize: 600 }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="about" className="about-section">
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-lg-6 order-last order-lg-first">
+                <div className="about-image text-center text-lg-end">
+                  <img src="assets/images/stripe.jpg" alt="StripePayment" />
+                </div>
+              </div>
+              <div className="col-lg-6 col-md-10">
+                <div className="about-content-wrapper space">
+                  <div className="numbers">3</div>
+                  <div className="section-title">
+                    <h2 className="mb-20">
+                      Pay Online for Dubai Visa Application
+                    </h2>
+                    <p className="mb-30">
+                      1. Pay online with credit or debit card <br />
+                      2. Google Pay <br />
+                      3. Apple Pay <br />
+                      <br />
+                    </p>
+                    <a href="#0" className="main-btn btn-hover">
+                      <i className="lni lni-whatsapp" /> WhatsApp Now !!
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="feature-2" className="feature-section-2 payonline">
+          <div className="feature-wrapper-2">
+            <div className="container">
+              <div className="row align-items-center">
+                <div className="col-lg-6 col-md-10">
+                  <div className="feature-content-wrapper space">
+                    <div className="numbers">4</div>
+                    <div className="section-title">
+                      <h2 className="mb-30">
+                        Get your <br className="d-none d-sm-block" />
+                        Dubai Visa Online
+                      </h2>
+                      <p className="mb-30">
+                        After visa approval by immigration, Express visa team
+                        will deliver you e-Visa copy by email and whatsapp. You
+                        can print the e-visa or just show pdf copy at airport
+                        counter to travel.
+                      </p>
+                      <a href="#0" className="main-btn btn-hover">
+                        <i className="lni lni-whatsapp" /> whatsApp Now!!
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="feature-image text-lg-end">
+                    <img
+                      src="assets/images/samplepassport.webp"
+                      alt="simplePassport"
+                      style={{ inlineSize: 600 }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="about" className="about-section">
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-lg-6 order-last order-lg-first">
+                <div className="about-image text-center text-lg-end">
+                  <img src="assets/images/news.jpg" alt="" />
+                </div>
+              </div>
+              <div className="col-lg-6 col-md-10">
+                <div className="about-content-wrapper space">
+                  <div className="numbers">5</div>
+                  <div className="section-title">
+                    <h2 className="mb-20">
+                      Print your Visa
+                      <br className="d-none d-sm-block" />
+                      Application and travel.
+                    </h2>
+                    <p className="mb-30">
+                      1. Pay online with credit or debit card <br />
+                      2. Google Pay <br />
+                      3. Apple Pay <br />
+                      <br />
+                    </p>
+                    <a href="#0" className="main-btn btn-hover">
+                      <i className="lni lni-whatsapp" /> WhatsApp Now !!
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="pricing" className="pricing-section priccing">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-xxl-8 col-xl-7 col-lg-8 col-md-10">
+                <div className="section-title text-center">
+                  <h2>
+                    Online Dubai Visa
+                    <br className="d-block" />
+                    Application Portal
+                  </h2>
+                  <p>Apply Dubai visa from anywhere online anytime.</p>
+                </div>
+              </div>
+            </div>
+            <div className="row align-items-center justify-content-center">
+              <div className="col-sm-10 items">
+                <div className="">
+                  <div className="pricing-body">
+                    <form>
+                      <div className="form__group">
+                        <div>
+                          <label htmlFor="Type">Visa Type</label>
+                          <div className="type__visa">
+                            <div>Single Entry</div>
+                            <div>Multi Entry</div>
+                          </div>
+                        </div>
+                        <div className="days__visa">
+                          <div>14 Days Visa</div>
+                          <div>30 Days Visa</div>
+                          <div>60 Days visa</div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+                <div className="single-pricing basic">
+                  <div className="pricing-header amount">
+                    <div className="time">
+                      <p>Processing Time</p>
+                      <div className="processing__time">
+                        <div className="radio__group">
+                          <input type="radio" name="normal" id="" />
+                          <p className="package-name">Normal (48 – 72 Hours)</p>
+                        </div>
+                        <div className="radio__group">
+                          <input type="radio" name="Express" id="" />
+                          <p className="package-name">Express (3-24 Hours)</p>
+                        </div>
+                      </div>
+                    </div>
+                    <h2 className="amount__">
+                      <span className="amoutn">125</span>
+                      <span className="currency">$</span>
+                    </h2>
+                  </div>
+                  <a href="#0" className="main-btn btn-hover">
+                    <i className="lni lni-whatsapp" /> WhatsApp Now !!
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <footer className="footer">
+          <div className="container">
+            <div className="copyright">
+              <p>Copyright 2024 OneClick Visa. All rights reserved.</p>
+            </div>
+          </div>
+        </footer>
+      </>
+    </>
+  );
+}
+
+export default App;
